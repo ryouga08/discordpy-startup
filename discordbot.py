4414f5e8e4b991e8b0a9f65e1ea962cd9afc4f9f
ryouga08 <72784989+ryouga08@users.noreply.github.com>
2020-10-13 19:28:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,7 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-embed = discord.Embed(title="タイトル",description="中身",color=0xff0000)
+embed = discord.Embed(title="タイトル",description="21\n22",color=0xff0000)
 embed.add_field(name="フィールドの名前",value="フィールドの値")
 
 @bot.event
@@ -24,9 +24,10 @@ async def ping(ctx):
 @bot.command()
 async def embcheck(ctx):
     msg = await ctx.channel.send(embed=embed)
-    await msg.add_reaction('\N{THUMBS UP SIGN}')
     await msg.add_reaction('\N{REGIONAL INDICATOR SYMBOL LETTER A}')
-    await msg.add_reaction('⏫')
+    await msg.add_reaction('\N{REGIONAL INDICATOR SYMBOL LETTER B}')
+    await msg.add_reaction('\N{REGIONAL INDICATOR SYMBOL LETTER C}')
+    await msg.add_reaction('\N{REGIONAL INDICATOR SYMBOL LETTER D}')
    
 
 
