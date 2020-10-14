5c1ac554a1c7c2faba29a837a7a5a2c5b0c44f2c
ryouga08 <72784989+ryouga08@users.noreply.github.com>
2020-10-14 17:24:06 +0900

    client()に頼る

--- discordbot.py
+++ discordbot.py
@@ -4,23 +4,9 @@ import discord
 import os
 import traceback
 
-client = discord.Client() #インスタンス化
-
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-embed = discord.Embed(title="タイトル",description="A\nB\nC\nD",color=0xff0000)
-embed.add_field(name="フィールドの名前",value="フィールドの値")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    
-@bot.command()
-async def embcheck(ctx):
-    msg = await ctx.channel.send(embed=embed)
-    for reaction in ['\N{REGIONAL INDICATOR SYMBOL LETTER A}', '\N{REGIONAL INDICATOR SYMBOL LETTER B}', '\N{REGIONAL INDICATOR SYMBOL LETTER C}','\N{REGIONAL INDICATOR SYMBOL LETTER D}']:
-        await msg.add_reaction(reaction)
+client = discord.Client() #インスタンス化
 
 @client.event 
 async def on_raw_reaction_add(payload):
@@ -31,6 +17,15 @@ async def on_raw_reaction_add(payload):
 async def on_message(msg):
     if msg.content == "/hello":
         await msg.channel.send("hello")
+        
+    if msg.content == "/embcheck":
+        embed = discord.Embed(title="タイトル",description="A\nB\nC\nD",color=0xff0000)
+        embed.add_field(name="フィールドの名前",value="フィールドの値")
+        em = await msg.channel.send(embed=embed)
+        for reaction in ['\N{REGIONAL INDICATOR SYMBOL LETTER A}', '\N{REGIONAL INDICATOR SYMBOL LETTER B}', '\N{REGIONAL INDICATOR SYMBOL LETTER C}','\N{REGIONAL INDICATOR SYMBOL LETTER D}']:
+            await em.add_reaction(reaction)
+
+        
        
             
 bot.run(token)
