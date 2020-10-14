a5fb49ada7fcc11bc120bf1727b3280c66420460
ryouga08 <72784989+ryouga08@users.noreply.github.com>
2020-10-14 18:24:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,7 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+rea = discord.Reaction()
 
 embed = discord.Embed(title="タイトル",description="A\nB\nC\nD",color=0xff0000)
 embed.add_field(name="フィールドの名前",value="フィールドの値")
@@ -20,6 +21,21 @@ async def embcheck(ctx):
     for reaction in ['\N{REGIONAL INDICATOR SYMBOL LETTER A}', '\N{REGIONAL INDICATOR SYMBOL LETTER B}', '\N{REGIONAL INDICATOR SYMBOL LETTER C}','\N{REGIONAL INDICATOR SYMBOL LETTER D}']:
         await msg.add_reaction(reaction)
         
+    def check(reaction, user):
+        emoji = str(reaction.emoji)
+        if user.bot == True:    # botは無視
+            pass
+        else:
+            return emoji == '\N{REGIONAL INDICATOR SYMBOL LETTER A}'
+    
+    reaction, user = await client.wait_for('reaction_add', check=check)
+    print(str(reaction.emoji))
+        if str(reaction.emoji) == '\N{REGIONAL INDICATOR SYMBOL LETTER A}':
+            reaction_member.append(user.name)
+            emded = discord.Embed(title="title",description="A\nB\nC\nD",colour=0xff0000)
+            embed.add_field(name = "name",value = "value")
+            await msg.edit(embed=embed)   
+    
 @bot.event
 async def on_message(message):
     # 送信者がbotである場合は弾く
@@ -29,11 +45,8 @@ async def on_message(message):
     if message.content == "hello":
         # メッセージが送られてきたチャンネルに送る
         await message.channel.send("hello")
+        
 
-@bot.event
-async def on_raw_reaction_add(payload):
-    if payload.emoji.name == '\N{REGIONAL INDICATOR SYMBOL LETTER A}': 
-        await payload.channel.send("hi")
         
 
             
