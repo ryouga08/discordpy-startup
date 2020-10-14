1e9553e4cf51b5128c024b47079894f204722862
ryouga08 <72784989+ryouga08@users.noreply.github.com>
2020-10-14 18:29:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,15 +7,14 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-embed = discord.Embed(title="タイトル",description="A\nB\nC\nD",color=0xff0000)
-embed.add_field(name="フィールドの名前",value="フィールドの値")
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
     
 @bot.command()
 async def embcheck(ctx):
+    embed = discord.Embed(title="タイトル",description="A\nB\nC\nD",color=0xff0000)
+    embed.add_field(name="フィールドの名前",value="フィールドの値")
     msg = await ctx.channel.send(embed=embed)
     for reaction in ['\N{REGIONAL INDICATOR SYMBOL LETTER A}', '\N{REGIONAL INDICATOR SYMBOL LETTER B}', '\N{REGIONAL INDICATOR SYMBOL LETTER C}','\N{REGIONAL INDICATOR SYMBOL LETTER D}']:
         await msg.add_reaction(reaction)
@@ -28,7 +27,7 @@ async def embcheck(ctx):
             return emoji == '\N{REGIONAL INDICATOR SYMBOL LETTER A}'
     
     reaction, user = await client.wait_for('reaction_add', check=check)
-    print(str(reaction.emoji))
+    # print(str(reaction.emoji))
         if str(reaction.emoji) == '\N{REGIONAL INDICATOR SYMBOL LETTER A}':
             reaction_member.append(user.name)
             emded = discord.Embed(title="title",description="A\nB\nC\nD",colour=0xff0000)
