a0cfff49428c78b2faaaa84bcca42d878c7815b2
ryouga08 <72784989+ryouga08@users.noreply.github.com>
2020-10-14 17:17:52 +0900

    error文消去

--- discordbot.py
+++ discordbot.py
@@ -12,13 +12,6 @@ token = os.environ['DISCORD_BOT_TOKEN']
 embed = discord.Embed(title="タイトル",description="A\nB\nC\nD",color=0xff0000)
 embed.add_field(name="フィールドの名前",value="フィールドの値")
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
