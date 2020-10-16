372820b0683257dec9f7e1f6e65b2bdef254e46b
ryouga08 <72784989+ryouga08@users.noreply.github.com>
2020-10-16 19:45:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,6 +9,21 @@ bot = commands.Bot(command_prefix='/')
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
+citycodes = {
+    "土浦": '080020',
+    "水戸": '080010',
+    "札幌": '016010',
+    "仙台": '040010',
+    "東京": '130010',
+    "横浜": '140010',
+    "名古屋": '230010',
+    "大阪": '270000',
+    "広島": '340010',
+    "福岡": '400010',
+    "鹿児島": '460010',
+    "那覇": '471010'
+}
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
@@ -50,17 +65,29 @@ async def length(ctx):
     wait_message = await client.wait_for("message",check=check)
     await ctx.send(wait_message.content)
     await ctx.send("end")
-   
-@bot.event
+ 
+@client.event
 async def on_message(message):
-    if message.content.startswith('$greet'):
-        channel = message.channel
-        await channel.send('Say hello!')
+  if message.author != client.user:
 
-        def check(m):
-            return m.content == 'hello' and m.channel == channel
+    reg_res = re.compile(u"Bot君、(.+)の天気は？").search(message.content)
+    if reg_res:
 
-        msg = await client.wait_for('message', check=check)
-        await channel.send('Hello {.author}!'.format(msg))
+      if reg_res.group(1) in citycodes.keys():
+
+        citycode = citycodes[reg_res.group(1)]
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%citycode).read()
+        resp = json.loads(resp.decode('utf-8'))
+
+        msg = resp['location']['city']
+        msg += "の天気は、\n"
+        for f in resp['forecasts']:
+          msg += f['dateLabel'] + "が" + f['telop'] + "\n"
+        msg += "です。"
+
+        await client.send_message(message.channel, message.author.mention + msg)
+
+      else:
+        await client.send_message(message.channel, "そこの天気はわかりません...")
             
 bot.run(token)
